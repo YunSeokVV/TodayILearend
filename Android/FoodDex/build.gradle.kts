3620bad89845713a2cd8f436e6aa5f9c0c66b467
yun_seok <agaroseson@naver.com>
2024-08-10 19:38:59 +0900

    [feat] 힐트 라이브러리 설정

--- Android/FoodDex/build.gradle.kts
+++ Android/FoodDex/build.gradle.kts
@@ -1,5 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
+    }
 }
