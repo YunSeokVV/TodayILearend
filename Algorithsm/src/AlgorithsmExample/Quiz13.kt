283b250a9d01b6f35b346de54493315db9518336
yunSeok <yunseok.jeong@varodrt.com>
2025-02-15 18:39:39 +0900

    [Quiz] 아직 못푼문제 제곱수 구하기

---
+++ Algorithsm/src/AlgorithsmExample/Quiz13.kt
@@ -0,0 +1,16 @@
+package AlgorithsmExample
+// https://school.programmers.co.kr/learn/courses/30/lessons/120909
+class Quiz13 {
+//    fun solution(n: Int): Int {
+//        var answer: Int = 0
+//
+//        Math.sqrt(n) % 1 ==
+//
+//        return answer
+//    }
+}
+
+fun main() {
+    val quiz = Quiz13()
+    //quiz.solution(144)
+}
