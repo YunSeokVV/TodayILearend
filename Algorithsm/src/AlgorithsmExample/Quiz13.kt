5102a7bdebbd924320beebfe2cc4e283be99686d
yunSeok <yunseok.jeong@varodrt.com>
2025-02-16 16:34:26 +0900

    [Quiz] 코테 문제풀이 - 코틀린 내장함수 두개 배움

--- Algorithsm/src/AlgorithsmExample/Quiz13.kt
+++ Algorithsm/src/AlgorithsmExample/Quiz13.kt
@@ -1,16 +1,21 @@
 package AlgorithsmExample
 // https://school.programmers.co.kr/learn/courses/30/lessons/120909
 class Quiz13 {
-//    fun solution(n: Int): Int {
-//        var answer: Int = 0
-//
-//        Math.sqrt(n) % 1 ==
-//
-//        return answer
-//    }
-}
+    fun solution(n: Int): Int {
+        var answer = 0
+
+        val tmp = Math.sqrt(n.toDouble())
+        if(tmp == tmp.toInt().toDouble()) {
+            answer = 1
+        } else {
+            answer = 2
+        }
+
+        return answer
+        }
+    }
 
 fun main() {
     val quiz = Quiz13()
-    //quiz.solution(144)
+    quiz.solution(144)
 }
